31c952cfed0189cb1f1553ae5617db12080543cf
Totiruzi <totiruzi@yahoo.com>
2020-09-25 13:51:03 +0300

    Add modal, form for streaming, history file and reducers for delete, create, edit and read

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,6 +1,55 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Modal from '../Modal';
+import history from '../../history';
+import { deleteStream, fetchStream } from '../../actions';
 
-const StreamDelete = () => {
-  return <div> StreamDelete</div>;
+class StreamDelete extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  renderActions = () => {
+    const { id } = this.props.match.params;
+    return (
+      <Fragment>
+        <button
+          className="ui button negative"
+          onClick={() => this.props.deleteStream(id)}
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </Fragment>
+    );
+  };
+
+  renderContent = () => {
+    if (!this.props.stream) {
+      return 'Are you sure you want to delete this stream?';
+    }
+    return `Are you sure you want to delete the stream with the title: ${this.props.stream.title}`;
+  };
+
+  render() {
+    return (
+      <Modal
+        title="Delete Stream"
+        content={this.renderContent()}
+        actions={this.renderActions()}
+        onDismiss={(e) => history.push('/')}
+      />
+    );
+  }
+}
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id],
+  };
 };
-export default StreamDelete;
+export default connect(mapStateToProps, { deleteStream, fetchStream })(
+  StreamDelete
+);
--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, editeStream } from '../../actions';
+import StreamForm from './StreamForm';
 
-const StreamEdit = () => {
-  return <div> StreamEdit</div>;
+class StreamEdit extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onFormSubmit = (formValues) => {
+    const { id } = this.props.match.params;
+    this.props.editeStream(id, formValues);
+  };
+  render() {
+    console.log(this.props);
+    if (!this.props.stream) {
+      return <div>Loading!!!!</div>;
+    }
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          // initialValues={{ title: 'this.props.stream.title', description: 'this.props.stream.description'}}
+          // using lodash to achive the same above purpose of initializing form values
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+          onFormSubmit={this.onFormSubmit}
+        />
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream, editeStream })(
+  StreamEdit
+);
